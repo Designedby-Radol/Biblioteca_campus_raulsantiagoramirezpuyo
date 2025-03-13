8722fc97166ea75cefab25beb5b43893cd5372b8
Raul Santiago Ramirez <139929916+Designedby-Radol@users.noreply.github.com>
2025-03-13 13:49:52 +0100

    Final commit

--- storage/mysql/db.sql
+++ storage/mysql/db.sql
@@ -4,10 +4,10 @@
 
     CREATE TABLE libros(
         id INT AUTO_INCREMENT PRIMARY KEY,
-        titulo VARCHAR(12) NOT NULL,
+        titulo VARCHAR(50) NOT NULL,
         isbn VARCHAR(10) NOT NULL, 
         descripcion VARCHAR(50),
-        genero VARCHAR(10) NOT NULL,
+        genero VARCHAR(50) NOT NULL,
         disponibilidad BOOLEAN NOT NULL
     );
 
@@ -20,44 +20,33 @@
     );
     CREATE TABLE autores(
         id INT AUTO_INCREMENT PRIMARY KEY ,
-        nombre VARCHAR(20)
+        nombre VARCHAR(50)
     );
 
-    CREATE TABLE prestamos(
+    CREATE TABLE transaccion(
         id INT AUTO_INCREMENT PRIMARY KEY ,
         fecha DATE,
         id_libro INT,
         id_miembros INT,
+        prestamo DATE,
+        devolucion DATE,
         FOREIGN KEY (id_libro) REFERENCES libros(id),
         FOREIGN KEY (id_miembros) REFERENCES miembros(id)
     );
 
-    CREATE TABLE devolucion(
-        id INT AUTO_INCREMENT PRIMARY KEY ,
-        fecha DATE,
-        id_prestamo INT,
-        FOREIGN KEY (id_prestamo) REFERENCES prestamos(id)
-    );
-
     CREATE TABLE editores(
         id INT AUTO_INCREMENT PRIMARY KEY ,
-        nombre VARCHAR(20)
+        nombre VARCHAR(50),
+        editor VARCHAR(50),
+        id_libro INT,
+        fechapublicacion DATE,
+        FOREIGN KEY (id_libro) REFERENCES libros(id)
     );
 
     CREATE TABLE libro_autor(
         id INT AUTO_INCREMENT PRIMARY KEY,
         id_autores INT,
-        id_libros INT,
-        id_publicaciones INT,
-        FOREIGN KEY (id_publicaciones) REFERENCES publicaciones(id),
+        id_libro INT,
         FOREIGN KEY (id_autores) REFERENCES autores(id),
-        FOREIGN KEY (id_libros) REFERENCES libros(id)
-    );
-
-    CREATE TABLE publicaciones(
-        id INT AUTO_INCREMENT PRIMARY KEY ,
-        fecha DATE,
-        nombre VARCHAR(20),
-        id_editores INT,
-        FOREIGN KEY (id_editores) REFERENCES editores(id)
+        FOREIGN KEY (id_libro) REFERENCES libros(id)
     );
--- storage/mysql/insert.sql
+++ storage/mysql/insert.sql
@@ -1,6 +1,24 @@
-INSERT INTO miembros (nombre, email, fecha_registro, edad) 
-VALUES ('Juan Pérez', 'juan@example.com', '2023-01-15', 18);
+INSERT INTO autores(id,nombre) VALUES
+(1,"gabriel garcia marquez"),
+(2,"rowland");
 
+INSERT INTO libros(id,titulo,genero,isbn,disponibilidad) VALUES
+(1,"cien años de soledad","realismo magico","12345678",TRUE),
+(2,"Harry popoter","accion","72830954",TRUE);
 
-INSERT INTO autores (nombre) VALUES ('juan patiño')
+INSERT INTO libro_autor(id,id_libro,id_autores) VALUES
+(1,1,1),
+(2,2,2);
+
+INSERT INTO miembros(id,nombre,email) VALUES
+(1,"Juan Pérez","juanperez123@gmail.com"),
+(2,"maria ramirez","mari@łł€jhags.com");
+
+INSERT INTO transaccion(id,id_libro,id_miembros,prestamo,devolucion) VALUES
+(1,1,1,"2025-03-01",NULL),
+(2,2,2,"2025-02-01",NULL);
+
+INSERT INTO editores(id,id_libro,editor,fechapublicacion) VALUES
+(1,1,"palito juan","2025-03-01"),
+(2,2,"pepganga","2025-02-01");
 
